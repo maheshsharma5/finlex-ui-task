6c43ff87783e106b40dfe43cf927380b64b618f0
maheshsharma5 <mahesh@metadesignsolutions.com>
2020-09-03 17:54:40 +0530

    Worked on fixed header update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,11 +12,25 @@ $(document).ready(function(){
         });
     } else {
         $('.js_setting_pop').click(function(){
-            $(this).parents('.floating-box').addClass('open')
+            $(this).parents('.floating-box').addClass('open');
+            $('body').addClass('modal-open');
         });
         $('.js_close').click(function(){
-            $(this).parents('.floating-box').removeClass('open')
+            $(this).parents('.floating-box').removeClass('open');
+            $('body').removeClass('modal-open');
         });
         
     }
-})
+});
+
+var eleTopPosition = $('.js-bannerposition').position().top + 82;
+$(window).on('scroll', function(){
+    if($(window).width() < 768){
+        var eleScrollTop = $(window).scrollTop();
+        if(eleScrollTop > eleTopPosition) {
+            $('body').addClass('scroll-element');
+        } else {
+            $('body').removeClass('scroll-element');
+        }
+    }
+});
